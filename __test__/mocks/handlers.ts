799353e444f818f47403d1866680d84b554192cb
dbehmoaras@gmail.com <dbehmoaras@gmail.com>
2021-09-14 11:59:50 -0400

    fixed the hanging server issue. hurrah! now it's completely ready to go. also fixed the jest config files so that they set up and tear down the MSW properly. key point: the jest setup and jest config files need to be in the root directory

--- __test__/mocks/handlers.ts
+++ __test__/mocks/handlers.ts
@@ -2,7 +2,6 @@ import { rest } from "msw";
 
 export const handlers = [
   rest.get("http://localhost:3001/test", (req, res, ctx) => {
-    console.log("***** HIT");
     return res(
       ctx.json({
         test: true,
